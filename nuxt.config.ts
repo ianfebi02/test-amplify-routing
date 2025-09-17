96ae6f024f4d4e6d925235aa2877ccf933a662e7
Ian Febi Sastrataruna <ianfebi01@Ians-MacBook-Air.local>
2025-09-17 22:34:48 +0700

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  nitro: {
+    awsAmplify: {
+      catchAllStaticFallback: false,
+    },
+    prerender : {
+      routes: [
+        "/",
+        "/test"
+      ],
+      failOnError: true, // Change to true to catch issues
+    },
+  },
+})
